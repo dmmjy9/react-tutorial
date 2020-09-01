b91565d99581d86d57bf199f765ffe2808a3b5b6
dmmjy9 <dmmjy9@gmail.com>
2020-09-02 00:58:54 +0800

    3-2.react design && event bind

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
 
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
